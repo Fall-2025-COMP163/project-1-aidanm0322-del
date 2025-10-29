2788c6d65414935dc60e14699af7b895f2cd8219
aidanm0322-del <aidanm0322@gmail.com>
2025-10-29 15:04:38 -0400

    aidan

---
+++ bs file2.py
@@ -0,0 +1,6 @@
+def calculate_rectangular_area(length,width):
+    area=length*width
+    return area
+print(calculate_rectangular_area(10,5))
+
+	    
--- project1_starter.py
+++ project1_starter.py
@@ -1,12 +1,12 @@
 """
 COMP 163 - Project 1: Character Creator & Saving/Loading
-Name: [Your Name Here]
-Date: [Date]
+Name: Aidan Miles 
+Date: 10-22-2025
 
 AI Usage: [Document any AI assistance used]
 Example: AI helped with file I/O error handling logic in save_character function
 """
-
+import os   
 def create_character(name, character_class):
     """
     Creates a new character dictionary with calculated stats
@@ -16,11 +16,15 @@ def create_character(name, character_class):
     char = create_character("Aria", "Mage")
     # Should return: {"name": "Aria", "class": "Mage", "level": 1, "strength": 5, "magic": 15, "health": 80, "gold": 100}
     """
-    # TODO: Implement this function
+     # TODO: Implement this function
     # Remember to use calculate_stats() function for stat calculation
-    pass
+    
+    character_creation={"name": name,"class": character_class,"level":1,"strength":5,"magic":15,"health":80,"gold":100}
+    
+    return character_creation
 
 def calculate_stats(character_class, level):
+   
     """
     Calculates base stats based on class and level
     Returns: tuple of (strength, magic, health)
@@ -33,7 +37,28 @@ def calculate_stats(character_class, level):
     """
     # TODO: Implement this function
     # Return a tuple: (strength, magic, health)
-    pass
+
+    if character_class == "Warrior":
+        strength = 20 + (level*6)
+        magic = 5 + (level*2)
+        health = 40 + (level*5)
+    elif character_class == "Mage":
+        strength = 5 + (level*2)
+        magic = 40 + (level*3)
+        health = 20 + (level*3)
+    elif character_class == "Rogue":
+        strength = 10 + (level*4)
+        magic = 15 + (level*3)
+        health = 30 + (level*4)
+    elif character_class == "Cleric":
+        strength = 30 + (level*3)
+        magic = 40 + (level*3)
+        health = 60 + (level*4)
+
+    else:
+        return (0,0,0)
+
+    return (strength,magic,health)
 
 def save_character(character, filename):
     """
@@ -49,20 +74,57 @@ def save_character(character, filename):
     Health: [health]
     Gold: [gold]
     """
+    if character == {} or filename == "":
+        return False
+
+    file = open(filename,"w")
+    file.write(f"Character Name:{character['name']}\n")
+    file.write(f"Class:{character['class']}\n")
+    file.write(f"Level:{character['level']}\n")
+    file.write(f"Strength:{character['strength']}\n")
+    file.write(f"Magic:{character['magic']}\n")
+    file.write(f"Health:{character['health']}\n")
+    file.write(f"Gold:{character['gold']}\n")
+    file.close()
+    return True
+
+
     # TODO: Implement this function
     # Remember to handle file errors gracefully
-    pass
+
 
 def load_character(filename):
     """
     Loads character from text file
     Returns: character dictionary if successful, None if file not found
     """
+    if not os.path.exists(filename):   
+        return None
+        
+    character={}
+    file=open(filename,"r")
+    #needed ai assitance with the key,value part of the for loop
+    for line in file:
+        key,value = line.strip().split(":") 
+        character[key] = value.strip() 
+    
+    file.close()
+    return character
+
+
+
     # TODO: Implement this function
     # Remember to handle file not found errors
-    pass
+    
 
 def display_character(character):
+    print("=== CHARACTER SHEET ===")
+    
+    
+    
+    
+    
+    
     """
     Prints formatted character sheet
     Returns: None (prints to console)
@@ -94,7 +156,11 @@ def level_up(character):
 if __name__ == "__main__":
     print("=== CHARACTER CREATOR ===")
     print("Test your functions here!")
-    
+    print(create_character("Aria","Mage"))
+    print(calculate_stats("Warrior",5))
+    print(calculate_stats("Mage",5))
+    print(calculate_stats("Rogue",5))
+    print(calculate_stats("Cleric",5))
     # Example usage:
     # char = create_character("TestHero", "Warrior")
     # display_character(char)
